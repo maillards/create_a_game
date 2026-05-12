f84d66a8dfef42c4e09743dbf3212979088a1cc9
rychelleL <567222@my.cuhsd.org>
2026-05-12 09:00:11 -0700

    Update alien_boss.py

--- alien_boss.py
+++ alien_boss.py
@@ -1,4 +1,5 @@
 import turtle as t
+# Imports random for code
 import random
 
 alien_img = "alien.png"
@@ -18,6 +19,7 @@ alien.penup()
 alien.goto(-190, 0)
 alien.showturtle()
 
+
 move_amount = 5
 
 laser = t.Turtle()
@@ -28,17 +30,26 @@ laser.penup()
 
 
 alien.speed(2)
+# Boolean to see if the alien is defeated or not
 defeated = False
 
+# Controls where the alien moves(up or down)
 step = 5
 
-while True:
+# While defeated is false
+while not defeated:
+  # Moves the alien up and down
   alien.sety(alien.ycor() + step)
   
+  # Makes sure the alien bounces back the opposite direction if it
+  #   goes too far
   if alien.ycor() > 120 or alien.ycor() < -120:
     step = step * -1
     
+  # Randomly shoots the laser if a random integer chosen out of 1 to 40
+      # Is equivalent to 1
   if random.randint(1, 40) == 1:
+    # Shoots the laser
     laser.goto(alien.xcor(), alien.ycor())
     laser.showturtle()
     laser.goto(300, alien.ycor())
